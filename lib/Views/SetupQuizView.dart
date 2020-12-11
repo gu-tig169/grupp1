13c0fef2ebe7dc3620cf29ef14e2340c7319c232
Ellinor Stomdal <gusstomel@student.gu.se>
2020-12-11 16:08:58 +0100

    created card in SetupQuizView

--- lib/Views/SetupQuizView.dart
+++ lib/Views/SetupQuizView.dart
@@ -7,6 +7,58 @@ class SetupQuizView extends StatelessWidget {
       appBar: AppBar(
         title: Text('SetupQuizView'),
       ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text('Choose Difficulty:'),
+          Text('Choose Category:'),
+          // _categoryList(context),
+        ],
+      ),
+    );
+  }
+/*
+  Widget _categoryList(context) {
+    return 
+  } */
+
+  Widget _categoryCard() {
+    return Center(
+      child: Card(
+        child: InkWell(
+          splashColor: Colors.blue.withAlpha(30),
+          onTap: () {
+            print('Tapped nature.');
+          },
+          child: Container(
+            width: 313,
+            height: 58,
+            child: Center(
+              child: Text(
+                'Nature',
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
+
+/* Widget extends 
+            DropdownButton<String>(             
+              value: dropdownValue,
+              icon: Icon(Icons.arrow_downward),
+              iconSize: 24,
+              elevation: 16,
+               onChanged: () {},
+               items: <String>['Easy', 'Medium', 'Difficult',]
+          .map<DropdownMenuItem<String>>((String value) {
+        return DropdownMenuItem<String>(
+          value: value,
+          child: Text(value),
+        );
+      }).toList(),
+    );
+  ) */
