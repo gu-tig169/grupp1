0d63b4e569302531b6284a06b0248e68236885cc
emeli <gusfolkem@student.gu.se>
2020-12-16 14:54:27 +0100

    added loggo & made a testquestion for UI

--- lib/Views/QuizView.dart
+++ lib/Views/QuizView.dart
@@ -1,12 +1,45 @@
 import 'package:flutter/material.dart';
 
 class QuizView extends StatelessWidget {
+  final QuestionItem testQuestion = QuestionItem('Grupp 1',
+      'Vem i grupp 1 har två katter?', 'Emelie', ['Ellinor', 'Erika', 'Alex']);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('QuizView'),
       ),
+      body: Center(
+        child: Column(
+          children: [
+            Text(testQuestion.category), //ersätt med ('$category')
+            _image(),
+            Text(testQuestion.question),
+            GridView.count(
+                crossAxisCount: 2,
+                children: List.generate(4, (index) {
+                  return Center(child: Card(child: Text('')));
+                }))
+          ],
+        ),
+      ),
     );
   }
+
+  Widget _image() {
+    return Center(child: Image(image: AssetImage('assets/logo.png')));
+  }
 }
+
+class QuestionItem {
+  final String category;
+  final String question;
+  final String correctAnswer;
+  final List<String> incorrectAnswer;
+
+  QuestionItem(
+      this.category, this.question, this.correctAnswer, this.incorrectAnswer);
+}
+
+//class Answer
