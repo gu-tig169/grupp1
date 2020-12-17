ed8ca878527690b1caa7216a863b34c4a6929e92
emeli <gusfolkem@student.gu.se>
2020-12-17 16:07:43 +0100

    added the top Container

--- lib/Views/ScoreView.dart
+++ lib/Views/ScoreView.dart
@@ -7,6 +7,52 @@ class ScoreView extends StatelessWidget {
       appBar: AppBar(
         title: Text('Score View'),
       ),
+      body: Center(
+        child: Column(children: [
+          _image(),
+          Container(
+              color: Colors.lightGreen,
+              width: 310,
+              height: 116,
+              child: Row(
+                children: [
+                  Column(children: [
+                    _placeHolderAvatar(),
+                    Text('Username'),
+                  ]),
+                  Container(
+                    width: 230,
+                    height: 116,
+                    color: Colors.blue,
+                    child: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text('BEST CATEGORY'),
+                          Text('Fashion'),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              )),
+        ]),
+      ),
+    );
+  }
+
+  Widget _image() {
+    return Center(child: Image(image: AssetImage('assets/logo.png')));
+  }
+
+  Widget _placeHolderAvatar() {
+    //Ta bort sedan!
+    return Center(
+      child: Image(
+        image: AssetImage('assets/logo.png'),
+        height: 80,
+        width: 80,
+      ),
     );
   }
 }
