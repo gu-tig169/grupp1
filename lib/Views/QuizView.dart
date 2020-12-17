53453c5253d72ce38144b721de8d955461692996
emeli <gusfolkem@student.gu.se>
2020-12-17 15:01:25 +0100

    changed the cards & added LinearProgressIndicator

--- lib/Views/QuizView.dart
+++ lib/Views/QuizView.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 
 class QuizView extends StatelessWidget {
-  final QuestionItem testQuestion = QuestionItem('Grupp 1',
-      'Vem i grupp 1 har två katter?', 'Emelie', ['Ellinor', 'Erika', 'Alex']);
+  final QuestionItem testQuestion = QuestionItem(
+    'Category',
+    'Vem i grupp 1 har två katter?',
+    'Emelie',
+    ['Ellinor', 'Erika', 'Alex'],
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -13,22 +17,75 @@ class QuizView extends StatelessWidget {
       body: Center(
         child: Column(
           children: [
-            Text(testQuestion.category), //ersätt med ('$category')
-            _image(),
-            Text(testQuestion.question),
-            GridView.count(
-                crossAxisCount: 2,
-                children: List.generate(4, (index) {
-                  return Center(child: Card(child: Text('')));
-                }))
-          ],
+            Row(
+              children: [
+                _image(),
+                Text(testQuestion.category),
+              ],
+            ),
+            Container(height: 10),
+            Padding(
+              padding: const EdgeInsets.only(left: 10.0, right: 10),
+              child: Container(
+                  height: 50,
+                  width: 330,
+                  child: Center(child: Text(testQuestion.question))),
+            ),
+            _answerCards(),
+            Container(height: 50),
+            _linearProgressIndicator()
+          ], //Column children
         ),
       ),
     );
   }
 
   Widget _image() {
-    return Center(child: Image(image: AssetImage('assets/logo.png')));
+    return Center(
+      child: Image(
+        image: AssetImage('assets/logo.png'),
+        height: 80,
+        width: 80,
+      ),
+    );
+  }
+
+  Widget _answerCards() {
+    return Column(
+      children: [
+        Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [_card(), _card()]),
+        Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [_card(), _card()]),
+      ],
+    );
+  }
+
+  Widget _card() {
+    return Card(
+        child: InkWell(
+            splashColor: Colors.blue.withAlpha(30),
+            onTap: () {},
+            child: Container(
+              height: 130,
+              width: 160,
+              child: Center(
+                child: Text('Answer'),
+              ),
+            )));
+  }
+
+  Widget _linearProgressIndicator() {
+    return SizedBox(
+      width: 330,
+      child: LinearProgressIndicator(
+        minHeight: 15,
+        backgroundColor: Colors.green[900],
+        valueColor: new AlwaysStoppedAnimation<Color>(Colors.deepPurple),
+      ),
+    );
   }
 }
 
@@ -41,5 +98,3 @@ class QuestionItem {
   QuestionItem(
       this.category, this.question, this.correctAnswer, this.incorrectAnswer);
 }
-
-//class Answer
